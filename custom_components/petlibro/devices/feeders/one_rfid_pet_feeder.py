5178378ee2c5bfda462aa948098619241f39cf08
C4-Dimitri <88452320+C4-Dimitri@users.noreply.github.com>
2024-08-26 21:10:59 +0100

    Test St

--- custom_components/petlibro/devices/feeders/one_rfid_pet_feeder.py
+++ custom_components/petlibro/devices/feeders/one_rfid_pet_feeder.py
@@ -10,34 +10,33 @@ class OneRFIDPetFeeder(Feeder):
         self.update_data({
             "grainStatus": await self.api.device_grain_status(self.serial)
         })
-    while True:
-        @property
-        def remaining_desiccant(self) -> str:
-            return cast(str, self._data.get("remainingDesiccantDays"))
 
-        @property
-        def today_feeding_quantity(self) -> int:
-            quantity = self._data.get("grainStatus", {}).get("todayFeedingQuantity")
-            if not quantity:
-                return 0
+    @property
+    def remaining_desiccant(self) -> str:
+        return cast(str, self._data.get("remainingDesiccantDays"))
 
-            return self.convert_unit(quantity)
+    @property
+    def today_feeding_quantity(self) -> int:
+        quantity = self._data.get("grainStatus", {}).get("todayFeedingQuantity")
+        if not quantity:
+            return 0
 
-        @property
-        def today_feeding_times(self) -> int:
-            return cast(int, self._data.get("grainStatus", {}).get("todayFeedingTimes"))
+        return self.convert_unit(quantity)
 
-        @property
-        def today_eating_time(self) -> int:
-            eating_time_str = self._data.get("grainStatus", {}).get("eatingTime", "0'0''")
-            if not eating_time_str:
-                return 0
+    @property
+    def today_feeding_times(self) -> int:
+        return cast(int, self._data.get("grainStatus", {}).get("todayFeedingTimes"))
 
-            try:
-                minutes, seconds = map(int, eating_time_str.replace("''", "").split("'"))
-                total_seconds = minutes * 60 + seconds
-            except ValueError as e:
-                return 0
+    @property
+    def today_eating_time(self) -> int:
+        eating_time_str = self._data.get("grainStatus", {}).get("eatingTime", "0'0''")
+        if not eating_time_str:
+            return 0
 
-            return total_seconds
-        time.sleep(5)
+        try:
+            minutes, seconds = map(int, eating_time_str.replace("''", "").split("'"))
+            total_seconds = minutes * 60 + seconds
+        except ValueError as e:
+            return 0
+
+        return total_seconds
