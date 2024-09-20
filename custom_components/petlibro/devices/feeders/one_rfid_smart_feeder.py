2f6bf9997da58a56fb77241b32c9eca74c15bc12
C4-Dimitri <88452320+C4-Dimitri@users.noreply.github.com>
2024-09-20 23:41:03 +0100

    Update custom_components/petlibro/devices/feeders/one_rfid_smart_feeder.py
    Co-authored-by: Florian Charlaix <github@flifloo.ovh>

--- custom_components/petlibro/devices/feeders/one_rfid_smart_feeder.py
+++ custom_components/petlibro/devices/feeders/one_rfid_smart_feeder.py
@@ -22,22 +22,6 @@ class OneRFIDSmartFeeder(GranaryFeeder):
         })
 
     @property
-    def remaining_desiccant(self) -> str:
-        return cast(str, self._data.get("remainingDesiccantDays"))
-
-    @property
-    def today_feeding_quantity(self) -> int:
-        quantity = self._data.get("grainStatus", {}).get("todayFeedingQuantity")
-        if not quantity:
-            return 0
-
-        return self.convert_unit(quantity)
-
-    @property
-    def today_feeding_times(self) -> int:
-        return cast(int, self._data.get("grainStatus", {}).get("todayFeedingTimes"))
-
-    @property
     def today_eating_time(self) -> int:
         eating_time_str = self._data.get("grainStatus", {}).get("eatingTime", "0'0''")
         if not eating_time_str:
