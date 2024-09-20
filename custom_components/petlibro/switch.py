16ee4327b08c954b5e4069b58a51ecc5e7e871c0
C4-Dimitri <88452320+C4-Dimitri@users.noreply.github.com>
2024-09-20 22:01:33 +0100

    Update custom_components/petlibro/switch.py
    Co-authored-by: Florian Charlaix <github@flifloo.ovh>

--- custom_components/petlibro/switch.py
+++ custom_components/petlibro/switch.py
@@ -47,6 +47,7 @@ DEVICE_SWITCH_MAP: dict[type[Device], list[PetLibroSwitchEntityDescription]] = {
     ]
 }
 
+
 class PetLibroSwitchEntity(PetLibroEntity[_DeviceT], SwitchEntity):
     """PETLIBRO switch entity."""
 
