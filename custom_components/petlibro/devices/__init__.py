3417dbe7a12a89374b7511cbb2457dd152f9aa27
C4-Dimitri <88452320+C4-Dimitri@users.noreply.github.com>
2024-09-20 21:55:22 +0100

    Update custom_components/petlibro/devices/__init__.py
    Co-authored-by: Florian Charlaix <github@flifloo.ovh>

--- custom_components/petlibro/devices/__init__.py
+++ custom_components/petlibro/devices/__init__.py
@@ -3,6 +3,7 @@ from .device import Device
 from .feeders.granary_feeder import GranaryFeeder
 from .feeders.one_rfid_smart_feeder import OneRFIDSmartFeeder
 
+
 product_name_map : Dict[str, Type[Device]] = {
     "Granary Feeder": GranaryFeeder,
     "One RFID Smart Feeder": OneRFIDSmartFeeder
