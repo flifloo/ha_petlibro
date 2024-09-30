2d9fddd036d5a1d6dd7d8a5fbe449393ca20ac5c
Jeremy Brown <jsb@jetcom.org>
2024-09-29 20:14:42 -0400

    Update __init__.py
    Added support for using Product ID instead of name

--- custom_components/petlibro/devices/__init__.py
+++ custom_components/petlibro/devices/__init__.py
@@ -7,3 +7,8 @@ product_name_map : Dict[str, Type[Device]] = {
     "Granary Feeder": GranaryFeeder,
     "Granary Camera Feeder": GranaryCameraFeeder
 }
+
+prodcut_id_map: Dict[str, Type[Device]] = {
+    "PLAF103": GranaryFeeder,
+    "PLAF203": GranaryCameraFeeder
+}
