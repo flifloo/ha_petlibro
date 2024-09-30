bd5ce3885bc6b9756cabd242946fdec384ea192b
Jeremy Brown <jsb@jetcom.org>
2024-09-30 12:05:22 -0400

    adding %, I think

--- custom_components/petlibro/sensor.py
+++ custom_components/petlibro/sensor.py
@@ -150,6 +150,7 @@ DEVICE_SENSOR_MAP: dict[type[Device], list[PetLibroSensorEntityDescription]] = {
             key="weight_percent",
             translation_key="weight_percent",
             icon="mdi:percent",
+            unit_of_measurement="%",
             device_class=SensorDeviceClass.MOISTURE,
         )
     ]
